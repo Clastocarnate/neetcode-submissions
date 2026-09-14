c2432da1a87b056d7fe3cdabeaf909acf9400c1f
Clastocarnate <128831242+Clastocarnate@users.noreply.github.com>
2026-09-14 17:28:51 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,18 @@
+#include<unordered_set>
+
+class Solution {
+public:
+
+    bool hasDuplicate(vector<int>& nums) {
+    unordered_set<int> mySet;
+    
+    for (int num:nums){
+        if (mySet.contains(num)) return true;
+
+        mySet.insert(num);
+    }
+    return false;    
+    
+    
+    }
+};
